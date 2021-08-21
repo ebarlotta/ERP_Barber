5686f1a63b4c7ef6fb46d42911370a7a07801b78
Enzo Barlotta <ebarlotta@yahoo.com.ar>
2021-08-21 00:05:39 -0300

    some fixes

--- resources/views/livewire/empresa-usuarios/empresa-usuarios-component.blade.php
+++ resources/views/livewire/empresa-usuarios/empresa-usuarios-component.blade.php
@@ -1,113 +1,118 @@
 <div>
-	<x-titulo>Relacionar Usuarios a Empresas</x-titulo>
-	<x-slot name="header">
-		<div class="flex">
-			<!-- //Comienza en submenu de encabezado -->
+    <x-titulo>Relacionar Usuarios a Empresas</x-titulo>
+    <x-slot name="header">
+        <div class="flex">
+            <!-- //Comienza en submenu de encabezado -->
 
-			<!-- Navigation Links -->
-			@livewire('submenu')
-		</div>
+            <!-- Navigation Links -->
+            @livewire('submenu')
+        </div>
 
-	</x-slot>
+    </x-slot>
 
-	<div class="content-center flex">
-		<div class="bg-white p-2 text-center rounded-lg shadow-lg w-full">
-			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-				<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
-					@if (session()->has('message'))
-						<div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
-							role="alert">
-							<div class="flex">
-								<div>
-									<p class="text-xm bg-lightgreen">{{ session('message') }}</p>
-								</div>
-							</div>
-						</div>
-					@endif
-					{{-- @if ($seleccionado) --}}
-					<div class="text-left">
-						<button wire:click="mostrarmodal()"
-							class="bg-green-300 hover:bg-green-400 text-white-900 font-bold py-2 px-4 rounded">
-							Relacionar nuevooooo
-						</button>
-					</div>
-				</div>
+    <div class="content-center flex">
+        <div class="bg-white p-2 text-center rounded-lg shadow-lg w-full">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+                    @if (session()->has('message'))
+                        <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
+                            role="alert">
+                            <div class="flex">
+                                <div>
+                                    <p class="text-xm bg-lightgreen">{{ session('message') }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    {{-- @if ($seleccionado) --}}
+                    <div class="text-left">
+                        <button wire:click="mostrarmodal()"
+                            class="bg-green-300 hover:bg-green-400 text-white-900 font-bold py-2 px-4 rounded">
+                            Relacionar nuevooooo
+                        </button>
+                    </div>
+                </div>
 
-				{{-- @endif --}}
-				@if ($isModalOpen)
-					@include('livewire.empresa-usuarios.createempresausuarios1')
-				@endif
-				@if ($empresas)
-					<table>
-						<tr>
-							<td>
-								<table>
-									@foreach ($empresas as $empresa)
-										<tr>
-											<td class="border px-4 py-2 text-left" wire:click="CargarUsuarios({{ $empresa->id }})">
-												
-												<div class="w-full p-3">
-													<div class="flex rounded overflow-hidden border @if ($seleccionado==$empresa->id) bg-red-300 @endif">
-														
-														{{ $seleccionado. " " . $empresa->id }}
-														<img class="block w-15 h-15 flex-none bg-cover"
-															src="https://picsum.photos/seed/picsum/100/100">
-														<div
-															class="bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
-															<div
-																class="text-black font-bold text-xl mb-2 leading-tight">
-																{{ $empresa->name }}</div>
-															<p class="text-grey-darker text-base">Read more
-															</p>
-														</div>
-													</div>
-												</div>
+                {{-- @endif --}}
+                @if ($isModalOpen)
+                    @include('livewire.empresa-usuarios.createempresausuarios')
+                @endif
+                @if ($empresas)
+                    <table>
+                        <tr>
+                            <td>
+                                <table>
+                                    @foreach ($empresas as $empresa)
+                                        <tr>
+                                            <td class="border px-4 py-2 text-left @if ($seleccionado==$empresa->id) bg-red-300 @endif" wire:click="CargarUsuarios({{ $empresa->id }})">
 
-											</td>
-										</tr>
-									@endforeach
-								</table>
-							</td>
-							<td>
-								<table>
-									@if ($usuariosdelaempresa)
-										@foreach ($usuariosdelaempresa as $usuario)
-											<tr>
-												<td class="border px-4 py-2 text-left bg-red-300">
-													<div class="w-full p-3">
-														<div class="flex rounded overflow-hidden border">
-															<img class="block w-15 h-15 flex-none bg-cover"
-																src="https://picsum.photos/seed/picsum/80/80">
-															<div
-																class="bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
-																<div class="text-black font-bold text-xl mb-2 leading-tight">{{ $usuario['name'] }}</div>
-																<p class="text-grey-darker text-base">Read more</p>
-															</div>
-															{{-- @if ($usuario->activo) --}}
-															<div
-																class="bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
-																<div
-																	class="text-black font-bold text-xl mb-2 leading-tight">
-																	<img class="block w-15 h-15 flex-none bg-cover"
-																		src="public/images/activo.jpg'">
-																</div>
-															</div>
-															{{-- @endif --}}
-														</div>
-													</div>
-												</td>
-											</tr>
-										@endforeach
-									@endif
-								</table>
-							</td>
-						</tr>
-					</table>
-				@else
-					<h1>No hay datos</h1>
-				@endif
-			</div>
-		</div>
-	</div>
+                                                <div class="w-full p-3">
+                                                    <div class="flex rounded overflow-hidden border">
+
+                                                        {{ $seleccionado . ' ' . $empresa->id }}
+                                                        <img class="block w-15 h-15 flex-none bg-cover"
+                                                            src="https://picsum.photos/seed/picsum/100/100">
+                                                        <div
+                                                            class="bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
+                                                            <div
+                                                                class="text-black font-bold text-xl mb-2 leading-tight">
+                                                                {{ $empresa->name }}</div>
+                                                            <p class="text-grey-darker text-base">Read more
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </td>
+                            <td>
+                                <div class="overflow-y-auto h-1/2">
+                                    <table>
+                                        @if ($usuariosdelaempresa)
+                                            @foreach ($usuariosdelaempresa as $usuario)
+                                                <tr>
+                                                    <td class="border px-4 py-2 text-left bg-red-300">
+                                                        <div class="w-full p-3">
+                                                            <div class="flex rounded overflow-hidden border">
+                                                                <img class="block w-15 h-15 flex-none bg-cover"
+                                                                    src="https://picsum.photos/seed/picsum/80/80">
+                                                                <div
+                                                                    class="bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
+                                                                    <div
+                                                                        class="text-black font-bold text-xl mb-2 leading-tight">
+                                                                        {{ $usuario['name'] }}</div>
+                                                                    <p class="text-grey-darker text-base">Read more and
+                                                                        more</p>
+                                                                </div>
+                                                                {{-- @if ($usuario->activo) --}}
+                                                                <div
+                                                                    class="bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
+                                                                    <div
+                                                                        class="text-black font-bold text-xl mb-2 leading-tight">
+                                                                        <img class="block w-15 h-15 flex-none bg-cover"
+                                                                            src="{{  asset('images/activo.jpg') }}" width="40" height="40">
+                                                                    </div>
+                                                                </div>
+                                                                {{-- @endif --}}
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        @endif
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                @else
+                    <h1>No hay datos</h1>
+                @endif
+            </div>
+        </div>
+    </div>
 </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 //use App\Http\Controllers\ClienteController;
 
-use App\Http\Controllers\EmpresaModuloController;
-use App\Http\Controllers\EmpresaUsuarioController;
 use App\Http\Livewire\Area\AreaComponent;
 use App\Http\Livewire\Empresa\EmpresaComponent;
 use App\Http\Livewire\Modulo\ModuloComponent;
@@ -13,7 +11,6 @@ use App\Http\Livewire\Proveedor\ProveedorComponent;
 use App\Http\Livewire\Cliente\ClienteComponent;
 use App\Http\Livewire\Empleado\EmpleadoComponent;
 use App\Http\Livewire\EmpresaUsuarios\EmpresaUsuariosComponent;
-use App\Models\EmpresaUsuario;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,9 +32,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-// Route::resource('cliente', ClienteController::class);
-
-//Route::resource('cliente', ClienteController::class)->names('cliente');
 Route::get('areas',AreaComponent::class)->name('areas');
 Route::get('cuentas',CuentaComponent::class)->name('cuentas');
 Route::get('proveedores',ProveedorComponent::class)->name('proveedores');
@@ -45,16 +39,8 @@ Route::get('clientes',ClienteComponent::class)->name('clientes');
 Route::get('empleados',EmpleadoComponent::class)->name('empleados');
 Route::get('empresausuarios',EmpresaUsuariosComponent::class)->name('empresausuarios');
 
-//Route::resource('empresausuarios/empresa',EmpresaUsuarioController::class)->names('relacionar');
-Route::get('empresausuario/{id}', [EmpresaUsuarioController::class,'index'])->name('relacionar');
-Route::get('empresausuario/{empresa_id}/{user_id}', [EmpresaUsuarioController::class,'anadirUsuarioEmpresa'])->name('anadirUsuarioEmpresa');
-
-//Route::get('empresausuarios',EmpresaUsuarioController::class)->name('relacionar');
-
 
 Route::get('empresas',EmpresaComponent::class)->name('empresas');
 Route::get('modulos',ModuloComponent::class)->name('modulos');
-// Route::get('modulos/{{empresa_id}}',ModuloComponent::class)->name('modulos');
-//Route::get( 'modulos/{empresa_id}', ['ModuloComponent@index']);
 
 
